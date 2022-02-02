23e7faaf69756b0096d4f20c25338a891accfc24
unknown <david.vieuxbled@outlook.fr>
2022-02-02 16:01:58 +0100

    Book's CRUD done

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Books;
+use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookController extends AbstractController
 {
@@ -17,10 +23,70 @@ class BookController extends AbstractController
     }
 
     #[Route('/book/listing', name: 'book_listing')]
-    public function index2(): Response
+    public function listing(EntityManagerInterface $entity): Response
     {
+        $books = $entity->getRepository(Books::class)->findAll();
+
         return $this->render('book/listing.html.twig', [
             'controller_name' => 'BookController',
+            'books' => $books
         ]);
     }
+
+    #[Route('/book/create', name: 'book_create')]
+    public function createBook(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $book = new Books();
+
+        $form = $this->createForm(BookType::class, $book);
+        $form->add("submit",SubmitType::class,["attr" => ["class" => "btn btn-primary"]]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('book_listing');
+        }
+
+        return $this->renderForm('book/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/book/edit/{id}', name: 'book_edit')]
+    public function updateBook(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Books::class)->find($id);
+
+        $form = $this->createForm(BookType::class,$book);
+        $form->add("submit",SubmitType::class,["attr" => ["class" => "btn btn-primary"]]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $entityManager->persist($book);
+            $entityManager->flush();
+            return $this->redirectToRoute('book_listing');
+        }
+        return $this->renderForm('book/create.html.twig', [
+            'controller_name' => 'Edit Book',
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/book/delete/{id}', name: 'book_delete')]
+    public function deleteBook(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Books::class)->find($id);
+        $entityManager->remove($book);
+        $entityManager->flush();
+        return $this->redirectToRoute('book_listing');
+        
+    }
+
 }
---
+++ src/Form/BookType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Books;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title',TextType::class,["label" => "Title"])
+            ->add('author',TextType::class,[])
+            ->add('summary',TextAreaType::class,[])
+            ->add('release_date',DateType::class, ['widget' => 'single_text'])
+            ->add('category',TextType::class,[])
+            ->add('for_child',CheckboxType::class,['required' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Books::class,
+        ]);
+    }
+}
