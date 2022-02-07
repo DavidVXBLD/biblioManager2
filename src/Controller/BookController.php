7a9b321724da99b21d3ee1967212f09a3d3f011f
unknown <david.vieuxbled@outlook.fr>
2022-02-07 01:36:04 +0100

    borrow functionning

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,30 +3,30 @@
 namespace App\Controller;
 
 use App\Entity\Books;
+use App\Entity\Borrow;
 use App\Form\BookType;
+use App\Entity\Clients;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookController extends AbstractController
 {
     #[Route('/book', name: 'book')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
-        ]);
+        
     }
 
     #[Route('/book/listing', name: 'book_listing')]
     public function listing(EntityManagerInterface $entity): Response
     {
         $books = $entity->getRepository(Books::class)->findAll();
-
         return $this->render('book/listing.html.twig', [
             'controller_name' => 'BookController',
             'books' => $books
@@ -89,4 +89,46 @@ class BookController extends AbstractController
         
     }
 
+    #[Route('/book/borrow/{id}', name: 'book_borrow')]
+    public function borrow(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Books::class)->findOneBy(['id'=>$id]);
+        
+        $borrow = new Borrow;
+            $borrow->setDateLoan(new \DateTime("now"));
+            $borrow->setDateRenderedMax(new \DateTime("+15 days"));
+            $borrow->setBooks($book);
+
+        $form = $this->createFormBuilder($borrow)
+        ->add("Clients", EntityType::class, [
+            "label" => "Emprunteur",
+            "class" => Clients::class,
+            "attr" => ['class' => 'form-control my-4'],
+            "choice_label" => 'lastName',
+        ])
+        ->add('save', SubmitType::class, [
+            'label' => "Valider",
+            "attr" => ["class' => 'btn btn-primary"]
+        ])
+        ->getForm();
+
+        $form -> handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $update = $form->getData();
+            
+            $book->setAvailable(1);
+
+            $borrow->setClients($update->getClients());
+
+            $entityManager->persist($borrow);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('book_listing');
+        } 
+        
+        return $this->renderForm('book/borrow.html.twig', [
+            'form' => $form
+        ]);
+    }
 }
